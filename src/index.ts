5556dee963c0a71160bdeb30cfdaa2d3337ceb9a
YuviPanda <yuvipanda@gmail.com>
2020-05-21 19:55:07 +0530

    Make eslint happy

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,12 @@ import {
 } from '@jupyterlab/application';
 
 import {
-  ICommandPalette, MainAreaWidget, WidgetTracker
+  ICommandPalette,
+  MainAreaWidget,
+  WidgetTracker
 } from '@jupyterlab/apputils';
 
-import {
-  Widget
-} from '@lumino/widgets';
-
+import { Widget } from '@lumino/widgets';
 
 class JitsiWidget extends Widget {
   private JitsiMeetExternalAPI: any;
@@ -23,7 +22,8 @@ class JitsiWidget extends Widget {
 
     this.addClass('jpe-jitsi-widget');
   }
-  async onAfterAttach() {
+
+  async onAfterAttach(): Promise<void> {
     console.log('attach called');
     if (!this.JitsiMeetExternalAPI) {
       this.JitsiMeetExternalAPI = await this.loadJitsiExternalApi();
@@ -31,14 +31,14 @@ class JitsiWidget extends Widget {
     // FIXME: How often is this actually called?
     const domain = 'meet.jit.si';
     const options = {
-        roomName: 'jupyterlab-extension-test-room',
-        parentNode: this.node
+      roomName: 'jupyterlab-extension-test-room',
+      parentNode: this.node
     };
 
     this.currentMeeting = new this.JitsiMeetExternalAPI(domain, options);
   }
 
-  onAfterDetach() {
+  onAfterDetach(): void {
     console.log('detach called');
     this.currentMeeting.dispose();
   }
@@ -48,21 +48,21 @@ class JitsiWidget extends Widget {
    *
    * Returns JitsiMeetExternalApi
    */
-  async loadJitsiExternalApi() {
-    console.log('jitsi loader called')
-    const SCRIPT_ID = "jpe-jitsi-external-api-loader-script";
+  async loadJitsiExternalApi(): Promise<void> {
+    console.log('jitsi loader called');
+    const SCRIPT_ID = 'jpe-jitsi-external-api-loader-script';
     return new Promise((resolve, reject) => {
       if (document.querySelector(`script#${SCRIPT_ID}`)) {
-        resolve((window as any).JitsiMeetExternalAPI)
+        resolve((window as any).JitsiMeetExternalAPI);
       } else {
         const script = document.createElement('script');
         // FIXME: Load this locally
         script.src = 'https://meet.jit.si/external_api.js';
         script.id = SCRIPT_ID;
-        script.onload = () => {
+        script.onload = (): void => {
           console.log('Loading Jitsi Meet External API script');
-          resolve((window as any).JitsiMeetExternalAPI)
-        }
+          resolve((window as any).JitsiMeetExternalAPI);
+        };
         // FIXME: Should this be appended to body instead? Does it matter?
         document.head.appendChild(script);
       }
@@ -70,25 +70,28 @@ class JitsiWidget extends Widget {
   }
 }
 
-async function activate(app: JupyterFrontEnd, palette: ICommandPalette, restorer: ILayoutRestorer) {
+async function activate(
+  app: JupyterFrontEnd,
+  palette: ICommandPalette,
+  restorer: ILayoutRestorer
+): Promise<void> {
   console.log('JupyterLab extension jupyter-jitsi is activated!');
   // Create a blank content widget inside of a MainAreaWidget
   let widget: MainAreaWidget<JitsiWidget>;
 
-  const command: string = 'jitsi:open';
+  const command = 'jitsi:open';
 
-
-  let tracker = new WidgetTracker<MainAreaWidget<JitsiWidget>>({
+  const tracker = new WidgetTracker<MainAreaWidget<JitsiWidget>>({
     namespace: 'jitsi'
   });
 
   app.commands.addCommand(command, {
     label: 'Jitsi Video Conference',
     execute: () => {
-      if(!widget || widget.isDisposed) {
+      if (!widget || widget.isDisposed) {
         // Create widget
         const content = new JitsiWidget();
-        widget = new MainAreaWidget({content});
+        widget = new MainAreaWidget({ content });
 
         widget.id = 'jitsi-jupyterlab';
         widget.title.label = 'Jitsi Video Conference';
@@ -101,7 +104,7 @@ async function activate(app: JupyterFrontEnd, palette: ICommandPalette, restorer
         // Attach the widget to the main work area if it's not there
         app.shell.add(widget, 'right');
       }
-      widget.content.update()
+      widget.content.update();
       // Activate the widget
       app.shell.activateById(widget.id);
     }
@@ -113,9 +116,8 @@ async function activate(app: JupyterFrontEnd, palette: ICommandPalette, restorer
   });
 
   // Add the command to the palette.
-  palette.addItem({command, category: 'Tutorial'});
+  palette.addItem({ command, category: 'Tutorial' });
 }
-
 
 /**
  * Initialization data for the jupyter-jitsi extension.
