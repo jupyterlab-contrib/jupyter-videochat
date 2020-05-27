15cb231d2914a15968208abe2f51375ba333a510
YuviPanda <yuvipanda@gmail.com>
2020-05-27 15:22:52 +0530

    Customize options available in Jitsi

--- src/sidebar.tsx
+++ src/sidebar.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 
 import JitsiMeetExternalAPI from  './external_api';
 import { ReactWidget, ToolbarButtonComponent, InputDialog } from '@jupyterlab/apputils';
+import { PageConfig } from '@jupyterlab/coreutils';
 
 import { listIcon } from '@jupyterlab/ui-components';
 
@@ -18,9 +19,29 @@ const JitsiMeetComponent = (props: JitsiMeetProps): JSX.Element => {
   useEffect(() => {
     const options = {
       roomName: props.meetingID,
-      parentNode: container.current
+      parentNode: container.current,
+      interfaceConfigOverwrite: {
+        // Overrides defined from https://github.com/jitsi/jitsi-meet/blob/master/interface_config.js
+        // We want to provide as simple an interface as possible, so we disable a bunch of things
+        // 1. Anything requiring an external login. So no recording, livestreaming or calendars
+        // 2. Anything that redirects to a mobile app. JupyterLab doesn't run on mobiles
+        // 3. Things we don't support - like etherpad, inviting, download, etc
+        TOOLBAR_BUTTONS: [
+          'microphone', 'camera', 'closedcaptions', 'desktop', 'fullscreen',
+          'fodeviceselection', 'hangup', 'profile', 'chat', /* 'recording', */
+          /* 'livestreaming', 'etherpad', */ 'sharedvideo', 'settings', 'raisehand',
+          'videoquality', 'filmstrip', /* 'invite', */ 'feedback', 'stats', 'shortcuts',
+          'tileview', 'videobackgroundblur', /* 'download', */ 'help', 'mute-everyone',
+          'e2ee', 'security'
+        ],
+        SETTINGS_SECTIONS: [ 'devices', 'language', 'moderator', 'profile', /* 'calendar' */ ],
+        // Users can't join with mobile app here
+        MOBILE_APP_PROMO: false
+      },
+      userInfo: {
+        displayName: PageConfig.getOption("hubUser") || undefined
+      }
     };
-    console.log(container)
 
     let currentMeeting = new JitsiMeetExternalAPI(props.domain, options);
     return () => {
