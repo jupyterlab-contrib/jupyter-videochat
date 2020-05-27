26b3f00c3857926e5650b93638d1596df40a9fa9
YuviPanda <yuvipanda@gmail.com>
2020-05-27 10:01:01 +0530

    Implement chat room chooser with React
    An unstyled dropdown that lets you pick which chat
    room to join. It uncermoniously drops you from the
    other chat room, which is probably ok for now.

--- src/sidebar.tsx
+++ src/sidebar.tsx
@@ -1,55 +1,41 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-import { Widget } from '@lumino/widgets';
 
 import JitsiMeetExternalAPI from  './external_api';
 import { ReactWidget } from '@jupyterlab/apputils';
 
-export class JitsiWidget extends Widget {
-
-  private currentMeeting: any;
-
-  constructor() {
-    super();
-
-    this.addClass('jpe-jitsi-widget');
-  }
-
-  async onAfterAttach(): Promise<void> {
-    console.log('attach called');
-    // FIXME: How often is this actually called?
-    const domain = 'meet.jit.si';
-    const options = {
-      roomName: 'jupyterlab-extension-test-room',
-      parentNode: this.node
-    };
-
-    // console.log((window as any).JitsiMeetexternalAPI)
-    this.currentMeeting = new JitsiMeetExternalAPI(domain, options);
-  }
-
-  onAfterDetach(): void {
-    console.log('detach called');
-    this.currentMeeting.dispose();
-  }
-}
-
 type JitsiMeetProps = {
-  meetingID: number
+  meetingID: string
+  domain: string
 }
 
 const JitsiMeetComponent = (props: JitsiMeetProps): JSX.Element => {
-  return (<div>
-    <span>{props.meetingID}</span>
+  let container = React.createRef<HTMLDivElement>();
+
+  useEffect(() => {
+    const options = {
+      roomName: props.meetingID,
+      parentNode: container.current
+    };
+    console.log(container)
+
+    let currentMeeting = new JitsiMeetExternalAPI(props.domain, options);
+    return () => {
+      console.log('disposing')
+      currentMeeting.dispose();
+    }
+  });
+
+  return (<div id="jp-videocall-jitsi-container" ref={container}>
   </div>)
 }
 
 const VideoCallSidebarComponent = (): JSX.Element => {
-  const [currentChat, setCurrentChat] = useState(1);
+  const [currentChat, setCurrentChat] = useState("project-1");
 
   return (
-    <div className="jp-videochat-container">
+    <div id="jp-videochat-container">
       <div>
         <label>
           Select chat to join
@@ -62,13 +48,12 @@ const VideoCallSidebarComponent = (): JSX.Element => {
           </select>
         </label>
       </div>
-      <JitsiMeetComponent meetingID={currentChat} />
+      <JitsiMeetComponent meetingID={currentChat} domain="meet.jit.si"/>
     </div>
   );
 }
 
 export class VideoCallSidebarWidget extends ReactWidget {
-  childWidget: JitsiWidget;
 
   constructor() {
     super()
